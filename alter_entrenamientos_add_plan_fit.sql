d1bf0ac61f683635aa35b518e427eb69960cb841
proyectoajpdsoft <alonsojpd@gmail.com>
2026-02-10 08:25:05 +0100

    Elimina definitivamente archivos de depuración y migración del repo público (migrados a dev_scripts y .gitignore)

--- alter_entrenamientos_add_plan_fit.sql
+++
@@ -1,6 +0,0 @@
--- Script para añadir el campo codigo_plan_fit a la tabla existente nu_entrenamientos
--- Ejecutar este script si ya tienes la tabla creada
-
-ALTER TABLE nu_entrenamientos 
-ADD COLUMN codigo_plan_fit INT NULL AFTER vueltas,
-ADD INDEX idx_codigo_plan_fit (codigo_plan_fit);
